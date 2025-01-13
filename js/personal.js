0ad0b2b7034fe1616cb418ed47d61fceadf80c66
dev60au <63218362+dev60au@users.noreply.github.com>
2025-01-13 14:17:20 +1000

    Update personal.js

--- js/personal.js
+++ js/personal.js
@@ -2,5 +2,5 @@ $(".r-phone").click(function(){
  $(".r-phone").html('<a href="tel:+61424224966">+61-424224966</a>');
 });
 $(".r-email").click(function(){
- $(".r-email").html('<a href="mailto:info@dev60.com">info@dev60.com</a>');
+ $(".r-email").html('<a href="mailto:info@SigmaLabs.com.au">info@SigmaLabs.com.au</a>');
 });
